884a769e49deba1458f38841b3878b526b2ff430
Layfyy <lucasalves@sapo.pt>
2024-05-23 21:22:42 +0100

    a
    a

--- project2/main.c
+++ project2/main.c
@@ -1,126 +1,80 @@
+// Created by lucas on 23/05/2024.
+
 #include <stdio.h>
 #include <stdlib.h>
-#include <math.h>
+#include <string.h>
+
+// Create node to store matrix
+typedef struct Node {
+    int value; // Node numeric value«
+    int column; // Node column
+    int row; // Node row
+} Node;
+
+Node *head = NULL;
 
 
-int numRows = 3;
-int numCols = 4;
-
-typedef struct Vertex {
-    int id;
-    int row;
-    int col;
-    int value;
-    struct Vertex* next;
-} Vertex;
-
+// Graph definition
 typedef struct Graph {
-    int number_of_nodes;
-    Vertex** adjacencyList;
+    Node node;
+    int id; // Node "id"
+    int numVertices; // Number of existing vertices
+    int numConnection; //  Number of connections made to the node
+
+    struct Graph* right;
+    struct Graph* down;
 } Graph;
 
-Vertex* createVertex(int id, int row, int col, int value) {
-    Vertex* newVertex = (Vertex*)malloc(sizeof(Vertex));
-    newVertex->id = id;
-    newVertex->row = id / numCols;
-    newVertex->col = col % numCols;
-    newVertex->value = value;
-    newVertex->next = NULL;
-    return newVertex;
-}
+void loadMatrixToList(char *filename) {
+    //Opens file in read mode
+    FILE *file = fopen(filename, "r");
 
-Graph* initializeGraph() {
-    Graph* graph = (Graph*)malloc(sizeof(Graph));
-    graph->number_of_nodes = numRows * numCols;
-    graph->adjacencyList = (Vertex**)malloc(graph->number_of_nodes * sizeof(Vertex*));
-    for (int i = 0; i < graph->number_of_nodes; i++) {
-        graph->adjacencyList[i] = NULL;
-    }
-    return graph;
-}
-
-void addEdge(Graph* graph, int src, int dest, int value) {
-    Vertex* newVertex = createVertex(dest, dest / graph->number_of_nodes + 1, dest % graph->number_of_nodes + 1, value);
-    newVertex->next = graph->adjacencyList[src];
-    graph->adjacencyList[src] = newVertex;
-}
-
-void readFile(Graph* graph) {
-    FILE* file = fopen("matrix.txt", "r");
+    //Verifies if the file was open
     if (file == NULL) {
-        printf("Failed to open file.\n");
-        exit(EXIT_FAILURE);
+        printf("Não foi possível abrir o arquivo %s.\n", filename);
+        return;
     }
 
+    //Reads line by line
+    //Each line is separated by a enter and each value by ";"
+    char line[256];
+    int row = 0;
+    while (fgets(line, sizeof(line), file)) {
 
-    int value;
-    int number_row = 0;
-    int number_col = 0;
+        //Token = number value
+        char *token;
+        token = strtok(line, ";");
+        int column = 0;
 
-    int id = 0;
-    while (fscanf(file, "%d,", &value) == 1) {
-        Vertex* newVertex = createVertex(id, number_row, number_col, value);
-        newVertex->next = graph->adjacencyList[id];
-        graph->adjacencyList[id] = newVertex;
+        while (token != NULL) {
 
-        // Adds new Vertex below current node
-        if (id / numCols < numRows - 1) {
-            addEdge(graph, id, id + numCols, value);
+            //Alloc space
+            Node *newNode = (Node *) malloc(sizeof(Node));
+
+            //Create node
+            newNode->value = atoi(token); //Value
+            newNode->column = column; //Column
+            newNode->row = row; //Row
+            head = newNode; //Head now points to new node
+
+            //Get next value
+            token = strtok(NULL, ";");
+            column++;
         }
-        // Adds new Vertex below to the right of current node
-        if (id % numCols < numCols - 1) {
-            addEdge(graph, id, id + 1, value);
-        }
 
-        id++;
-        number_col++;
-        if (number_col == numCols) {
-            number_row++;
-            number_col = 0;
-        }
+        //Passar para a proxima linha
+        row++;
     }
-
+    //Fechar o ficheiro
     fclose(file);
 }
 
-void printGraphAsMatrix(Graph* graph) {
-    int counter = 0;
-    printf("\n");
+// Create graph
+Graph *createGraph(int numVertices, int maxNumConnections);
 
-    for (int i = 0; i <= graph->number_of_nodes; i++) {
-        Vertex* temp = graph->adjacencyList[i];
-        if(temp == NULL){
-            printf("0 ");
-        } else {
-            printf("%d ", temp->value);
-        }
-        counter++;
-        if (counter == numCols) {
-            printf("\n");
-            counter = 0;
-        }
-    }
-}
-
-void printGraph(Graph* graph) {
-    for (int i = 0; i < graph->number_of_nodes; i++) {
-        // Gets the adjacencyList and atributes it to temp
-        Vertex* temp = graph->adjacencyList[i];
-        // Prints Node id
-        printf("\nAdjacency list of node %d\n head ", i);
-        while (temp) {
-            // Prints Adjacents nodes id's
-            printf("-> %d", temp->id);
-            temp = temp->next;
-        }
-        printf("\n");
-    }
-}
 
 int main() {
-    Graph* graph = initializeGraph();
-    readFile(graph);
-    printGraph(graph);
-    printGraphAsMatrix(graph);
+    loadMatrixToList("matrix.txt");
+
     return 0;
 }
